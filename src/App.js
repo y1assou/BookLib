7bdccce0b215bdf7ad17a8726473cbe0f5f73f74
y1assou <yassertazarine@gmail.com>
2024-07-18 10:35:25 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary modules and components
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './Main_component/Main';
+import Home from './Home/Home';
+import About from './About/About';
+import Catalogue from './Catalogue/Catalogue';
+import Books from './Catalogue/Categorie/Books/Books';
+import Contact from './Contact/Contact';
+import Sign from './Sign/Sign';
+import Add from './Add_book/Add';
+
+import Admin from './Admin/Admin';
+import Main from './Admin/Main_compenent/Main';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+
+        {/* Main Layout Routes */}
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="catalogue" element={<Catalogue />} />
+          <Route path="books/:category" element={<Books />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="sign" element={<Sign />} />
+          <Route path="addBook" element={<Add />} />
+        </Route>
+
+        <Route path="/admin" element={<Main/>}>
+          <Route index element={<Admin />} />
+        </Route>
+
+      </Routes>
+    </Router>
   );
 }
 
